2dac4c06f6f01adfc9c8c053fcb1edd567b08412
Sarkis-A <austin.sarkis@outlook.com>
2025-05-11 19:52:12 -0600

    Updated main.py with text retrieving function.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+# Gets the contents of a file
+def get_book_text(file_path):
+    with open(file_path) as book:
+        book_contents = book.read()
+        return book_contents
+
+# Defines the main function
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    print(book_text)
+
+main()
