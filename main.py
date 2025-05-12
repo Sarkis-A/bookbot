f6dfe406de3fe9f3a376e385d885427c1e0309d6
Sarkis-A <austin.sarkis@outlook.com>
2025-05-11 20:00:33 -0600

    Added content split function to main.py

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ def get_book_text(file_path):
     with open(file_path) as book:
         book_contents = book.read()
         return book_contents
+    
+# Splits the text of the file
+def content_split(file_contents):
+    words = file_contents.split()
+    return words
 
 # Defines the main function
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
-    print(book_text)
+    words_in_book = content_split(book_text)
+    word_count = len(words_in_book)
+    print(f"{word_count} words found in the document")
 
 main()
