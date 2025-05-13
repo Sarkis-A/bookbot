e57aadfb5a065c646e957ba8ce8bf4625c11950e
Sarkis-A <austin.sarkis@outlook.com>
2025-05-12 20:25:11 -0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_char
+from stats import get_num_words, get_num_char, sort_dict
 
 # Gets the contents of a file
 def get_book_text(file_path):
@@ -8,10 +8,18 @@ def get_book_text(file_path):
 
 # Defines the main function
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    book_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     word_count = get_num_words(book_text)
-    char_count = get_num_char(book_text)
-    print(f"{word_count} words found in the document")
-    print(char_count)
+    sorted_char_list = sort_dict(get_num_char(book_text))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path[2:]}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in sorted_char_list:
+        print(f"{i["char"]}: {i["count"]}")
+    print("============= END ===============")
 
 main()
